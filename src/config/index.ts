f3bb3233e42a86c543c7641e22ea4135c2630841
JanhaviGhatkar26 <janhavig@fermion.in>
2025-01-06 15:21:18 +0530

    Code pushin to the host

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,13 +1,13 @@
 import dotenv from "dotenv";
 
 dotenv.config({
-  path: "config/.env",
+  path: "src/config/.env",
 });
 
-export const MONGO_URL = `mongodb://localhost:27017/${
-  process.env.DB_NAME || "online_food_delivery"
-}`;
-
+// export const MONGO_URL = `mongodb://localhost:27017/${
+//   process.env.DB_NAME || "online_food_delivery"
+// }`;
+export const MONGO_URL = `${process.env.MONGODB_URL}${process.env.DB_NAME}`;
 export const SECRATE_KEY: string = process.env.SECRATE_KEY || "";
 export const PORT = process.env.PORT || 8000;
 export const TWILIOACCOUNTSID = process.env.TWILIOACCOUNTSID;
