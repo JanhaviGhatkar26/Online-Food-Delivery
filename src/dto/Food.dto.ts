b6d48be287aab0f69a4a9d8f3453dfa8711f3269
JanhaviGhatkar26 <janhavig@fermion.in>
2025-02-12 12:06:50 +0530

    shopping working

--- src/dto/Food.dto.ts
+++ src/dto/Food.dto.ts
@@ -1,8 +1,45 @@
+import {
+  IsArray,
+  IsEnum,
+  IsNumber,
+  IsOptional,
+  IsString,
+  Min,
+} from "class-validator";
+
 export interface CreateFoodInput {
   name: string;
   description: string;
-  category: string;
+  category: string[];
   foodType: string;
   readyTime: number;
   price: number;
+  image: string[];
+}
+export class CreateFoodDto {
+  @IsString()
+  name: string;
+
+  @IsString()
+  description: string;
+
+  @IsArray()
+  @IsString({ each: true })
+  category: string[];
+
+  @IsEnum(["Veg", "Non-Veg", "Vegan"], { message: "Invalid food type" })
+  foodType: string;
+
+  @IsOptional()
+  @IsNumber()
+  readyTime: number;
+
+  @IsNumber()
+  @Min(1)
+  price: number;
+
+  @IsOptional()
+  @IsArray()
+  @IsString({ each: true })
+  images: string[];
 }
